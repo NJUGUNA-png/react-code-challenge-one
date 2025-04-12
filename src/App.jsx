18243f5a807c3de15c14c8a40e0cdd5001902ece
NJUGUNA NJOROGE <engbrian01@gmail.com>
2025-04-12 18:17:25 +0300

    created an array of expenses and used state to store all expenses

--- src/App.jsx
+++ src/App.jsx
@@ -3,3 +3,28 @@ import './App.css'
 import ExpenseForm from "./ExpenseForm";
 import ExpenseList from "./ExpenseList";
 
+function App(){
+  const [expenses, setExpenses]= useState([
+    {
+      id: 1,
+      description:"Gym Membership",
+      amount: 100,
+      date: "2025-04-12",
+      category: "Health"
+    },
+    {
+      id: 2,
+      description:"Monthly rent",
+      amount:1500,
+      date: "2025-04-30",
+      category: "Housing"
+    },
+    {
+      id:3,
+      description:"Gas Bill",
+      amount: 60,
+      date: "2025-04-15",
+      category: "Utilities"
+    }
+  ])
+}
