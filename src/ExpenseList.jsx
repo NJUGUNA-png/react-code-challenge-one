5b55f169f9b3e38caf047f64b963a2275addc05e
NJUGUNA NJOROGE <engbrian01@gmail.com>
2025-04-12 17:25:46 +0300

    created a handlesearchchange function

--- src/ExpenseList.jsx
+++ src/ExpenseList.jsx
@@ -8,4 +8,10 @@ function ExpenseList({expenses, onDeleteExpense}){
         key: null,
         direction: 'ascending'
     });
+
+    function handleSearchChange(event){
+        setSearchTerm(event.target.value);
+    }
+
+    
 }
